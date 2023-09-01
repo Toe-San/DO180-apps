f27bea5ad429fbbdf1e3b06487ea03ee4e95cb9b
TOESAN <67867354+Toe-San@users.noreply.github.com>
2023-09-01 11:32:37 +0630

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,13 +1,15 @@
 <?php
-$string= "powerranger";
-echo $string."<br>";
+$string = "powerranger";
+echo $string . "<br>";
 
-//Change the fontsize using PHP, HTML and css through echo
-echo '<span style="font-size: 50px;"> ' . $string.   '</span>';
+// Change the fontsize using PHP, HTML, and CSS through echo
+echo '<span style="font-size: 50px;"> ' . $string . '</span>';
 
-//Change the font color using PHP, HTML and css through echo
+// Change the font color using PHP, HTML, and CSS through echo
 echo "<br>";
-echo '<span style="color: red; font-size: 50px;"> ' . $string.   '</span>';
+echo '<span style="color: red; font-size: 50px;"> ' . $string . '</span>';
 
-<img src=”https://github.com/Toe-San/DO180-apps/blob/master/php-helloworld/tawtar.PNG”>
+// Corrected the img tag syntax and URL
+echo '<img src="https://github.com/Toe-San/DO180-apps/blob/master/php-helloworld/tawtar.PNG">';
 ?>
+
