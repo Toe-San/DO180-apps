44d37df688de335512e1e6e94e27b0dbfb6a43ef
TOESAN <67867354+Toe-San@users.noreply.github.com>
2023-09-01 11:29:55 +0630

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -8,4 +8,6 @@ echo '<span style="font-size: 50px;"> ' . $string.   '</span>';
 //Change the font color using PHP, HTML and css through echo
 echo "<br>";
 echo '<span style="color: red; font-size: 50px;"> ' . $string.   '</span>';
+
+<img src=”https://github.com/Toe-San/DO180-apps/blob/master/php-helloworld/tawtar.PNG”>
 ?>
